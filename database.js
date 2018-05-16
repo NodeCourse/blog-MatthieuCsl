b8165d930e609f8f3c0ccf305fb0c3896108b3a4
MatthieuCsl <matthieucasoli@hotmail.fr>
2018-05-16 12:31:29 +0200

    affichage articles + voteup

--- database.js
+++ database.js
@@ -5,12 +5,23 @@ const db = new Sequelize('nodejs_blog', 'root', '', {
 
 });
 
+//setup des models
 const Article = db.define('article', {
   title: { type: Sequelize.STRING },
   description: { type: Sequelize.STRING },
-  content: { type: Sequelize.STRING },
-  photo: { type: Sequelize.STRING }
+  content: { type: Sequelize.STRING }
 });
+
+const Vote = db.define('vote', {
+  type: {
+    type: Sequelize.ENUM('up', 'down')
+  }
+});
+
+//definition des relations
+Article.hasMany(Vote);
+Vote.belongsTo(Article);
 
 module.exports.db = db;
 module.exports.Article = Article;
+module.exports.Vote = Vote;
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const db = require('./database');
 
 
+
 // Use Pug to render views
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
 // Serve assets from the public folder
 app.use(express.static('public'));
 
@@ -16,9 +19,38 @@ app.get('/', (req,res) => {
     })
     .then((articles) => {
       res.render('homepage', {
-        articles: articles
+        articles
       });
     });
+});
+
+app.get('/createArticle', (req, res) => {
+  res.render('/createArticle');
+});
+
+app.get('/freshArticle', (req, res) =>{
+  res.render('/freshArticle');
+});
+
+//creation de la route post pour un article
+app.post('/api/article', (req, res) => {
+  const title = req.body.title;
+  db.Article
+    .create({ title: title })
+    .then(() => {
+      res.redirect('/');
+    });
+});
+
+app.post('/api/article/:articleId/upvote', (req, res) => {
+  db.Vote.sync()
+    .then(() => {
+      db.Vote.create({
+      type: 'up',
+      articleId: req.params.articleId
+    });
+  res.redirect('/');
+  });
 });
 
 app.listen(3000);
